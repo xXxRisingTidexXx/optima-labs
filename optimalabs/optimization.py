149499d85e457598be73301e933dc24b80a9e323
xXxRisingTidexXx <danylopetrik@gmail.com>
2023-04-23 15:39:28 +0300

    Added optimize code

---
+++ optimalabs/optimization.py
@@ -0,0 +1,208 @@
+from enum import Enum, unique
+from typing import NamedTuple, Callable, Optional
+from numpy import ndarray, eye, asarray, sum as asum, arange, abs, sign, zeros, sqrt
+from numpy.linalg import norm
+
+
+def main():
+    print('ralgb5a')
+    print('-' * 50, end='\n\n')
+    print(ralgb5a(mse, asarray([[12], [-3]])))
+    print(ralgb5a(rosenbrock, asarray([[0], [3]])))
+    print(ralgb5a(rosenbrock, asarray([[0], [3]]), h0=2.25))
+    print(ralgb5a(piecewise_linear, zeros((100, 1), dtype=float), h0=10.0, q1=1.0, max_iter=5000))
+    print(ralgb5a(piecewise_linear2, zeros((20, 1), dtype=float), h0=10.0, q1=1.0))
+    print('\n\nemshor')
+    print('-' * 50, end='\n\n')
+    print(emshor(mse, asarray([[12], [-3]]), 64))
+    print(emshor(rosenbrock, asarray([[0], [3]]), 64))
+    print(emshor(piecewise_linear, zeros((100, 1), dtype=float), 64, 1e-6, max_iter=500000))
+    print(emshor(piecewise_linear2, zeros((20, 1), dtype=float), 64, max_iter=20000))
+
+
+def mse(x: ndarray) -> tuple[float, ndarray]:
+    n = x.shape[0]
+    return asum(x ** 2) / n, x * 2 / n
+
+
+def rosenbrock(u: ndarray) -> tuple[float, ndarray]:
+    a, b = 1, 100
+    x, y = u[0, 0], u[1, 0]
+    f = (a - x) ** 2 + b * (y - x ** 2) ** 2
+    g = asarray([[2 * (x - a) - 4 * b * x * (y - x ** 2)], [2 * b * (y - x ** 2)]])
+    return f, g
+
+
+def piecewise_linear(x: ndarray) -> tuple[float, ndarray]:
+    q = 1.2 ** arange(x.shape[0]).reshape(-1, 1)
+    x1 = x - 1
+    return (q.T @ abs(x1)).item(), q * sign(x1)
+
+
+def piecewise_linear2(x: ndarray) -> tuple[float, ndarray]:
+    q = 2 ** arange(x.shape[0]).reshape(-1, 1)
+    x1 = x - 1
+    return (q.T @ abs(x1)).item(), q * sign(x1)
+
+
+def ralgb5a(
+    func: 'Func',
+    x0: ndarray,
+    alpha: float = 4.0,
+    h0: float = 1.0,
+    q1: float = 0.9,
+    eps_x: float = 1e-10,
+    eps_g: float = 1e-12,
+    max_iter: int = 500
+) -> 'Result':
+    if len(x0.shape) != 2 or x0.shape[1] != 1:
+        raise ValueError(f'Vector x0 must have a shape of (n, 1), got {x0.shape}')
+    if alpha < 1:
+        raise ValueError(f'Expected alpha > 1, got {alpha}')
+    if eps_x <= 0:
+        raise ValueError(f'Eps for x must be positive, got {eps_x}')
+    if eps_g <= 0:
+        raise ValueError(f'Eps for g must be positive, got {eps_g}')
+    if max_iter < 1:
+        raise ValueError(f'Must be at least 1 iteration, got {max_iter}')
+    x_r, n_iter, n_calls = x0.astype(float), 0, 1
+    f_r, g = func(x_r)
+    if x_r.shape != g.shape:
+        raise ValueError(f'Gradient must have a shape of {x0.shape}, got {g.shape}')
+    if norm(g) < eps_g:
+        return Result(x_r, n_iter, n_calls, ExitCode.eps_g)
+    x, h_s = x_r, h0
+    b = eye(x.shape[0])
+    m = (1 / alpha - 1)
+    n_iter = 1
+    while n_iter <= max_iter:
+        dx = b @ normalize(b.T @ g)
+        regulation = regulate_h(func, x, h_s, dx, x_r, f_r, q1, eps_x, eps_g)
+        x, h_s, x_r, f_r = regulation.x, regulation.h_s, regulation.x_r, regulation.f_r
+        n_calls += regulation.n_calls
+        if regulation.exit_code is not None:
+            return Result(x_r, n_iter, n_calls, regulation.exit_code)
+        eta = normalize(b.T @ (regulation.g - g))
+        b += m * b @ eta @ eta.T
+        g = regulation.g
+        n_iter += 1
+    return Result(x_r, n_iter, n_calls, ExitCode.max_iter)
+
+
+Func = Callable[[ndarray], tuple[float, ndarray]]
+
+
+class Result(NamedTuple):
+    x_r: ndarray
+    n_iter: int
+    n_calls: int
+    exit_code: 'ExitCode'
+
+
+@unique
+class ExitCode(Enum):
+    eps_f = 1
+    eps_g = 2
+    eps_x = 3
+    max_iter = 4
+    error = 5
+
+
+def normalize(x: ndarray) -> ndarray:
+    return x / norm(x)
+
+
+def regulate_h(
+    func: Func,
+    x0: ndarray,
+    h_s0: float,
+    dx: ndarray,
+    x_r0: ndarray,
+    f_r0: float,
+    q1: float,
+    eps_x: float,
+    eps_g: float
+) -> 'Regulation':
+    x, h_s = x0, h_s0
+    g = asarray([], dtype=float)
+    x_r, f_r = x_r0, f_r0
+    d, ddx = 1, 0
+    l2 = norm(dx)
+    n_steps, n_calls = 0, 0
+    while d > 0:
+        x -= h_s * dx
+        ddx += h_s * l2
+        f, g = func(x)
+        n_calls += 1
+        if f < f_r:
+            x_r, f_r = x, f
+        if norm(g) < eps_g:
+            return Regulation(x, h_s, g, x_r, f_r, n_calls, ExitCode.eps_g)
+        n_steps += 1
+        if n_steps % 3 == 0:
+            h_s *= 1.1
+        if n_steps > 500:
+            return Regulation(x, h_s, g, x_r, f_r, n_calls, ExitCode.error)
+        d = dx.T @ g
+    return Regulation(
+        x,
+        h_s * q1 if n_steps == 1 else h_s,
+        g,
+        x_r,
+        f_r,
+        n_calls,
+        ExitCode.eps_x if ddx < eps_x else None
+    )
+
+
+class Regulation(NamedTuple):
+    x: ndarray
+    h_s: float
+    g: ndarray
+    x_r: ndarray
+    f_r: float
+    n_calls: int
+    exit_code: Optional[ExitCode]
+
+
+def emshor(
+    func: Func,
+    x0: ndarray,
+    r0: float,
+    eps_f: float = 1e-9,
+    max_iter: int = 500
+) -> Result:
+    if len(x0.shape) != 2 or x0.shape[1] != 1:
+        raise ValueError(f'Vector x0 must have a shape of (n, 1), got {x0.shape}')
+    if r0 <= 0:
+        raise ValueError(f'Radius r0 must be positive, got {r0}')
+    if eps_f <= 0:
+        raise ValueError(f'Eps for f must be positive, got {eps_f}')
+    if max_iter < 1:
+        raise ValueError(f'Must be at least 1 iteration, got {max_iter}')
+    x, n_iter, n_calls = x0.astype(float), 0, 0
+    n, r_n = x.shape[0], r0
+    f, g = func(x)
+    if x.shape != g.shape:
+        raise ValueError(f'Gradient must have a shape of {x0.shape}, got {g.shape}')
+    beta = sqrt((n - 1) / (n + 1))
+    b = eye(n)
+    while n_iter < max_iter:
+        f, g = func(x)
+        n_calls += 1
+        g = b.T @ g
+        d_g = norm(g)
+        if r_n * d_g < eps_f:
+            return Result(x, n_iter, n_calls, ExitCode.eps_f)
+        x_i = (1 / d_g) * g
+        d_x = b @ x_i
+        h_s = r_n / (n + 1)
+        x -= h_s * d_x
+        b += (beta - 1) * b @ x_i @ x_i.T
+        r_n *= n / sqrt(n * n - 1)
+        n_iter += 1
+    return Result(x, n_iter, n_calls, ExitCode.max_iter)
+
+
+if __name__ == '__main__':
+    main()
---
+++ optimalabs/planning.py
@@ -0,0 +1,200 @@
+from numpy import (
+    ndarray,
+    diag,
+    asarray,
+    zeros,
+    inf,
+    repeat,
+    ones,
+    vstack,
+    insert,
+    maximum,
+    sum,
+    reshape,
+    where,
+    full
+)
+from scipy.optimize import Bounds, LinearConstraint, OptimizeResult, minimize
+from optimalabs.optimization import Func, ExitCode, ralgb5a, emshor
+
+SUCCESS_CODES = {ExitCode.eps_f, ExitCode.eps_g, ExitCode.eps_x}
+
+
+def minimize_planning_constrained(
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray
+) -> OptimizeResult:
+    m, n = len(b), len(c)
+    x0 = full(n, s // n)
+    x0[: s % n] += 1
+    bounds = Bounds(zeros(n), full(n, inf))
+    constraint = LinearConstraint(
+        vstack([ones((1, n)), a]),
+        insert(zeros(m), 0, s, 0),
+        insert(b, 0, s, 0)
+    )
+    return minimize(
+        planning_objective,
+        x0,
+        (c, d),
+        'trust-constr',
+        planning_jacobian,
+        planning_hessian,
+        bounds=bounds,
+        constraints=constraint
+    )
+
+
+def planning_objective(x: ndarray, c: ndarray, d: ndarray) -> float:
+    return c @ x + d @ x ** 2
+
+
+def planning_jacobian(x: ndarray, c: ndarray, d: ndarray) -> ndarray:
+    return c + 2 * d * x
+
+
+def planning_hessian(_x: ndarray, _c: ndarray, d: ndarray) -> ndarray:
+    return diag(2 * d)
+
+
+def minimize_planning_with_penalty(
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray
+) -> OptimizeResult:
+    n = len(c)
+    x0 = full(n, s // n)
+    x0[: s % n] += 1
+    return minimize(
+        planning_with_penalty_objective,
+        x0,
+        (c, d, s, a, b),
+        'BFGS',
+        planning_with_penalty_jacobian
+    )
+
+
+def planning_with_penalty_objective(
+    x: ndarray,
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray,
+    mu: float = 1e6
+) -> float:
+    alpha = sum(maximum(a @ x - b, 0) ** 2) + sum(maximum(-x, 0) ** 2) + (sum(x) - s) ** 2
+    return planning_objective(x, c, d) + mu * alpha
+
+
+def planning_with_penalty_jacobian(
+    x: ndarray,
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray,
+    mu: float = 1e6
+) -> ndarray:
+    g = repeat(reshape(a @ x - b, (-1, 1)), len(c), 1)
+    alpha = 2 * sum(maximum(g, 0) * where(g >= 0, a, 0), 0)
+    alpha += 2 * maximum(-x, 0) * where(x <= 0, -1, 0) + 2 * (sum(x) - s)
+    return planning_jacobian(x, c, d) + mu * alpha
+
+
+def minimize_planning_ralgb5a(
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray
+) -> OptimizeResult:
+    n = len(c)
+    x0 = full((n, 1), s // n)
+    x0[: s % n] += 1
+    result = ralgb5a(make_planning_with_penalty_joint(c, d, s, a, b), x0)
+    return OptimizeResult(
+        x=result.x_r.reshape(-1),
+        success=(result.exit_code in SUCCESS_CODES),
+        status=result.exit_code.value,
+        message=result.exit_code.name,
+        fun=planning_with_penalty_objective(result.x_r, c, d, s, a, b),
+        nit=result.n_iter,
+        nfev=result.n_calls,
+        njev=result.n_calls
+    )
+
+
+def make_planning_with_penalty_joint(
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray,
+    mu: float = 1e6
+) -> Func:
+    def func(x: ndarray) -> tuple[float, ndarray]:
+        x0 = x.reshape(-1)
+        f = planning_with_penalty_objective(x0, c, d, s, a, b, mu)
+        g = planning_with_penalty_jacobian(x0, c, d, s, a, b, mu)
+        return f, g.reshape((-1, 1))
+
+    return func
+
+
+def minimize_planning_emshor(
+    c: ndarray,
+    d: ndarray,
+    s: float,
+    a: ndarray,
+    b: ndarray
+) -> OptimizeResult:
+    n = len(d)
+    x0 = full((n, 1), s // n)
+    x0[: s % n] += 1
+    result = emshor(make_planning_with_penalty_joint(c, d, s, a, b), x0, 1e2, 1e-2)
+    return OptimizeResult(
+        x=result.x_r.reshape(-1),
+        success=(result.exit_code in SUCCESS_CODES),
+        status=result.exit_code.value,
+        message=result.exit_code.name,
+        fun=planning_with_penalty_objective(result.x_r, c, d, s, a, b),
+        nit=result.n_iter,
+        nfev=result.n_calls,
+        njev=result.n_calls
+    )
+
+
+def main():
+    c = asarray([0.6, 2.6, 9.15])
+    d = asarray([0.19, 0.08, 1.5])
+    s = 11
+    a = asarray(
+        [
+            [2.567, 7.21, 0.12],
+            [1.98, 7.3, 6.21],
+            [0.31, 2.721, 8.12],
+            [1.372, 4.203, 2.91]
+        ]
+    )
+    b = asarray([95, 150.67, 23.76, 61.21])
+    print('SciPy, trust-region, bounds and linear constraints\n')
+    print(minimize_planning_constrained(c, d, s, a, b))
+    print('-' * 100)
+    print('SciPy, BFGS, penalty function\n')
+    print(minimize_planning_with_penalty(c, d, s, a, b))
+    print('-' * 100)
+    print('Ours, ralgb5a, penalty function\n')
+    print(minimize_planning_ralgb5a(c, d, s, a, b))
+    print('-' * 100)
+    print('Ours, emshor, penalty function\n')
+    print(minimize_planning_emshor(c, d, s, a, b))
+
+
+if __name__ == '__main__':
+    main()
